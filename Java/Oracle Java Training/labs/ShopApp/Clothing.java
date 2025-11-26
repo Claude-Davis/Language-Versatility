009c1fa6d8619ff534ce64935669825c8ed85650
Claudé Davis <claudedavis2024@gmail.com>
2025-11-26 11:47:44 -0500

    updates to lab

---
+++ Java/Oracle Java Training/labs/ShopApp/Clothing.java
@@ -0,0 +1,18 @@
+public class Clothing {
+    String description, size="M";
+    double price;
+
+    //setter
+    public void setDesc(String d){
+        this.description=d;}
+    public void setPrice(double p){
+        this.price=p;}
+    public void setSize(String s){
+        this.size=s;}
+
+    //getter
+    public double getPrice(){return price;}
+
+    //toString
+    public String toString(){return "This is a size " +size+ ", " +description+ " selling for $" +price+ ".";}
+}
--- Java/Oracle Java Training/labs/ShopApp/Main.java
+++ Java/Oracle Java Training/labs/ShopApp/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to the Shop!");
 
+        //tax
+        double tax=0.2;
+        //total cost
+        double total=0.0;
+
         //create a customer obj
         Customer c1 = new Customer("Jaimie");
         //greet customer
@@ -10,5 +15,26 @@ public class Main {
         c1.setName("Johnny");
         //greet customer
         c1.greeting();
+
+        System.out.println(" ");
+
+        //create clothing objs
+        Clothing item1= new Clothing();
+        Clothing item2= new Clothing();
+        //set clothing obj properties
+        item1.setDesc("Blue Jacket");
+        item2.setDesc("Orange T-Shirt");
+        item1.setPrice(20.9);
+        item2.setPrice(10.5);
+        item2.setSize("S");
+        //print the info for the clothingobjs
+        System.out.println(item1.toString() + " " + item2.toString());
+
+        System.out.println(" ");
+
+        //total price calculation
+        total += tax*(item1.getPrice()+(2*item2.getPrice())) + (item1.getPrice()+(2*item2.getPrice()));
+        System.out.println("$"+total);
+
     }
 }
